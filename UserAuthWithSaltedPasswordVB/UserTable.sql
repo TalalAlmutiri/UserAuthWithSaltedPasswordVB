939a9a0436bd0d78edcf1193dabec342aea290ca
TalalAlmutiri <62042702+TalalAlmutiri@users.noreply.github.com>
2020-08-10 00:31:23 +0300

    Add files via upload

---
+++ UserAuthWithSaltedPasswordVB/UserTable.sql
@@ -0,0 +1,11 @@
+CREATE TABLE [dbo].[UsersInfo](
+	[Username] [varchar](20) NOT NULL,
+	[HashedPwd] [varchar](200) NULL,
+	[Salt] [varchar](100) NULL,
+ CONSTRAINT [PK_UsersInfo] PRIMARY KEY CLUSTERED 
+(
+	[Username] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
